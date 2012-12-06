b50fa9ab2676f145da3177065100e814042db9ea
Brian Manning <brian@xaoc.org>
2012-12-05 21:29:28 -0800

    uncrufted perl template from factoid_tool.pl

--- program_stubs/perl-template.pl
+++ program_stubs/perl-template.pl
@@ -1,14 +1,13 @@
 #!/usr/bin/perl -w
 
-# Copyright (c) 2010 by Brian Manning <elspicyjack at gmail dot com>
-# PLEASE DO NOT E-MAIL THE AUTHOR ABOUT THIS SCRIPT!
+# Copyright (c) 2012 by Brian Manning <elspicyjack at gmail dot com>
 # For help with script errors and feature requests,
-# contact someone.
+# please contact the author on IRC;
+# irc.freenode.net #kernel-panic, nick: spicyjack
 
 =head1 NAME
 
-B<template.pl> - A template file with many Perl classes and a C<main> class
-for running.
+B<template.pl> - Browse, maintain and dump C<Infobot> factoid files.
 
 =head1 VERSION
 
@@ -25,26 +24,25 @@ our $VERSION = '0.01';
  Script options:
  -v|--verbose       Verbose script execution
  -h|--help          Shows this help text
- -c|--config        Configuration file to use for script options
 
  Other script options:
- -a|--password      password
- -u|--user          username
+ -f|--file          Path to factoid file; may be used multiple times
+ -d|--dump          Dump the factoid file(s) as key/value pairs to STDOUT
+ -l|--list          Pretty-print key/value pairs to STDOUT
+ -k|--keys          List factoid keys
+ -s|--values        List factoid values
 
  Example usage:
 
- # Generate a config file to modify that contains the script defaults
- template.pl --verbose
-
- # Use a configuration file for script options
- template.pl --config /path/to/config/file.cfg
+ # list factoids in /path/to/factoid-is.[dir|pag]
+ template.pl --file /path/to/factoid-is --list
 
 You can view the full C<POD> documentation of this file by calling C<perldoc
 template.pl>.
 
 =head1 DESCRIPTION
 
-B<template.pl> is a template file to be used for writing scripts.
+B<template.pl> - Browse, maintain and dump C<Infobot> factoid files.
 
 =head1 OBJECTS
 
@@ -75,13 +73,6 @@ L<Getopt::Long>.
 
 =cut
 
-# a list of valid arguments to this script
-my @_valid_script_args = ( qw(verbose config) );
-); # my @_valid_script_args
-
-# a list of arguments that won't cause the script to barf if Shout is not
-# installed
-
 sub new {
     my $class = shift;
 
@@ -99,121 +90,23 @@ sub new {
         # script options
         q(verbose|v+),
         q(help|h),
-        q(config|c=s),
         # other options
-        q(password|a=s),
-        q(user|u=s),
+        q(file|f=s@),
+        q(dump|d),
+        q(list|l),
+        q(keys|k),
+        q(values|s),
     ); # $parser->getoptions
 
     # assign the args hash to this object so it can be reused later on
     $self->{_args} = \%args;
 
-    # a check to verify the shout module is available
-    # it's put here so some warning is given if --help was called
-
-=begin comment
-
-    BEGIN {
-        eval q( use Shout; );
-        if ( $@ ) {
-            if ( defined grep(/-h|--help|-j|--gen-config/, @ARGV) ) {
-                warn qq(\nWARNING: Shout Perl module is not installed!\n\n);
-            } else {
-                warn qq( ERR: Shout module not installed\n);
-                warn qq( ERR: === Begin error output ===\n\n);
-                warn qq($@\n);
-                warn qq( ERR: === End error output ===\n);
-                die qq(Missing 'Shout' Perl module; exiting...);
-            } # if ( $self->get(q(help)) )
-        } # if ( $@ )
-    } # BEGIN
-
-=end comment
-
-=cut
-
     # dump and bail if we get called with --help
     if ( $self->get(q(help)) ) { pod2usage(-exitstatus => 1); }
-
-    # generate a config file and exit?
-    if ( defined $self->get(q(gen-config)) ) {
-        # apply the default configuration options to the Config object
-        $self->_apply_defaults();
-        # now print out the sample config file
-        print qq(# sample template config file\n);
-        print qq(# any line that starts with '#' is a comment\n);
-        print qq(# sample config generated on )
-            . POSIX::strftime( q(%c), localtime() ) . qq(\n);
-        foreach my $arg ( @_valid_shout_args ) {
-            print $arg . q( = ) . $self->get($arg) . qq(\n);
-        } # foreach my $arg ( @_valid_shout_args )
-        # cheat a bit and add these last config settings
-        # here document syntax
-        print <<EOC;
-# more config file parameters here
-key1 = value1
-# commenting the logfile will log to STDOUT instead
-logfile = /path/to/output.log
-EOC
-        exit 0;
-    } # if ( exists $args{gen-config} )
-
-    # read a config file if that's specified
-    if ( defined $self->get(q(config)) && -r $self->get(q(config)) ) {
-        open( CFG, q(<) . $self->get(q(config)) );
-        my @config_lines = <CFG>;
-        my $config_errors = 0;
-        foreach my $line ( @config_lines ) {
-            chomp $line;
-            warn qq(VERB: parsing line '$line'\n)
-                if ( defined $self->get(q(verbose)));
-            next if ( $line =~ /^#/ );
-            my ($key, $value) = split(/\s*=\s*/, $line);
-            warn qq(VERB: key/value for line is '$key'/'$value'\n)
-                if ( defined $self->get(q(verbose)));
-            if ( grep(/$key/, @_valid_script_args) > 0 ) {
-                $self->set($key => $value);
-            } else {
-                warn qq(WARN: unknown config line found in )
-                    . $self->get(q(config)) . qq(\n);
-                warn qq(WARN: unknown config line key/value: $key/$value\n);
-                $config_errors++;
-            } # if ( grep($key, @_valid_shout_args) > 0 )
-        } # foreach my $line ( @config_lines )
-        if ( defined $self->get(q(check-config)) ) {
-            warn qq|Found $config_errors total config error(s)\n|;
-            warn qq(Exiting script...\n);
-            exit 0;
-        } # if ( defined $self->get(q(check-config)) )
-    } # if ( exists $args{config} && -r $args{config} )
-
-=begin comment
-
-    # some checks to make sure we have needed arguments
-    die qq( ERR: script called without --config or --filelist arguments;\n)
-        . qq( ERR: run script with --help switch for usage examples\n)
-        unless ( defined $self->get(q(filelist)) );
-
-=end comment
-
-=cut
-
-    # apply script defaults to whatver remaining key/value pairs don't have
-    # anything set
-    $self->_apply_defaults();
 
     # return this object to the caller
     return $self;
 } # sub new
-
-# set defaults here for any missing arugments
-sub _apply_defaults {
-    my $self = shift;
-    # icecast defaults
-    $self->set( user => q(source) ) unless ( defined $self->get(q(user)) );
-    $self->set( password => q(default) ) unless (
-        defined $self->get(q(password)) );
-} # sub _apply_defaults
 
 =item get($key)
 
@@ -381,6 +274,7 @@ C<undef> if the file doesn't exist on the filesystem or can't be read.
 package Template::File;
 use strict;
 use warnings;
+use DBM::Deep;
 
 =over
 
@@ -414,6 +308,7 @@ sub new {
         _logger => $logger,
         _config => $config,
         _filename => $filename,
+        _db => undef,
     }, $class);
 
     # some tests of the actual file on the filesystem
@@ -460,22 +355,7 @@ use warnings;
     $logger->timelog(qq(INFO: Starting template.pl, version $VERSION));
     $logger->timelog(qq(INFO: my PID is $$));
 
-    # reroute some signals to our handlers
-    # exiting the script
-    $SIG{INT} = $SIG{TERM} = sub {
-        my $signal = shift;
-        $logger->timelog(qq(CRIT: Received SIG$signal; exiting...));
-        # close the connection to the icecast server
-        $conn->close();
-    }; # $SIG{INT}
-
-    $SIG{HUP} = sub {
-        $logger->timelog(qq(INFO: Received SIGHUP;));
-    }; # $SIG{HUP}
-
-    $SIG{USR1} = sub {
-        $logger->timelog(qq(INFO: Received SIGUSR1;));
-    }; # $SIG{USR1}
+    # XXX actual script contents goes here
 
 =head1 AUTHOR
 
@@ -484,7 +364,7 @@ Brian Manning, C<< <brian at xaoc dot org> >>
 =head1 BUGS
 
 Please report any bugs or feature requests to
-C<< <a_mailing_list at example com> >>.
+C<< <irc://irc.freenode.com/#kernel-panic spicyjack> >>.
 
 =head1 SUPPORT
 
@@ -494,7 +374,7 @@ You can find documentation for this script with the perldoc command.
 
 =head1 COPYRIGHT & LICENSE
 
-Copyright (c) 2008,2010,2012 Brian Manning, all rights reserved.
+Copyright (c) 2012 Brian Manning, all rights reserved.
 
 This program is free software; you can redistribute it and/or modify it
 under the same terms as Perl itself.
