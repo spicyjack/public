9b9787b9383bc307cb161092cd2ad3b863a4d5c4
Brian Manning <brian@xaoc.org>
2013-06-22 07:47:55 -0700

    vagrant/bootstrap: fixed whitespace

--- rc_scripts/vagrant/bootstrap.sh
+++ rc_scripts/vagrant/bootstrap.sh
@@ -5,9 +5,9 @@ aptitude update
 aptitude upgrade
 echo "Installing base system packages"
 apt-get --assume-yes install git vim-nox screen less bzip2 zip unzip \
-	sysv-rc-conf stow libgtk2-perl kernel-package debhelper dh-make-perl
+    sysv-rc-conf stow libgtk2-perl kernel-package debhelper dh-make-perl
 su - vagrant
 mkdir src
 cd src
 git clone git clone https://github.com/spicyjack/rcfiles-lack.git \
-	rcfiles-lack.git
+    rcfiles-lack.git
